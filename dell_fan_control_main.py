ebfc4cf86097c9d03018b78bed1012b0db94008f
kostage <crckostage@gmail.com>
2020-02-21 01:51:31 +0300

    debug off

--- dell_fan_control_main.py
+++ dell_fan_control_main.py
@@ -7,7 +7,7 @@ import time
 
 from fan_control_system import FanControlSystem
 
-DEBUG_STUBS = True
+DEBUG_STUBS = False
 
 if __name__ == '__main__':
     fan_control_system = FanControlSystem('config.yml', DEBUG_STUBS)
